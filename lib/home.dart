a131baa5f7970dec084bb9ea6b6087e719dc80ac
Karthick <karthiram165@gmail.com>
2020-05-07 19:52:39 +0530

    build things

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: Center(
-          child: Text(
-        sayHello(),
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.deepOrange, fontSize: 30),
-      )),
-    );
-  }
-
-  String sayHello() {
-    String hello;
-    DateTime now = new DateTime.now();
-    int hour = now.hour;
-    if (hour < 12) {
-      hello = "Good morning";
-    } else if (hour < 18) {
-      hello = "Good Afternoon";
-    } else {
-      hello = "Good evening";
-    }
-    return hello;
+    return Center(
+        child: Container(
+      child: Row(
+        children: [
+          Expanded(
+              child: Text(
+            'Panneer big Pizza!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 30.0, decoration: TextDecoration.none),
+          )),
+          Expanded(
+              child: Text(
+            'Panneer small Pizza!',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 30.0, decoration: TextDecoration.none),
+          )),
+        ],
+      ),
+      alignment: Alignment.center,
+      color: Colors.deepOrangeAccent,
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
-import './home.dart';
 
 void main() {
-  runApp(new HelloFlutterApp());
+  runApp(new Myapp());
 }
 
-class HelloFlutterApp extends StatelessWidget {
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "hello flutter",
-        home: Scaffold(appBar: AppBar(title: Text('Karthick')), body: home()));
+    return MaterialApp(title: 'My app', home: new HelloYou());
+  }
+}
+
+class HelloYou extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HelloYoustate();
+}
+
+class _HelloYoustate extends State<HelloYou> {
+  String name = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('helo'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (String string) {
+                setState(() {
+                  name = string;
+                });
+              },
+            ),
+            Text('Hello ' + name + ' !')
+          ],
+        ),
+      ),
+    );
   }
 }
